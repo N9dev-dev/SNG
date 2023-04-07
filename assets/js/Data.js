ca4c3ab016fdb01fc3db0efc6b19b9985dd2f24a
N9dev-dev <81910680+N9dev-dev@users.noreply.github.com>
2023-04-07 05:42:53 +0400

    edits

---
+++ assets/js/Data.js
@@ -0,0 +1,97 @@
+// lynx
+
+presets = {
+    // Rainbow
+    1: {
+        colors: ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "9400D3"]
+    },
+    // King
+    2: {
+        colors: ["b3a100", "00a8b3"]
+    },
+    // Quality
+    3: {
+        colors: ["00FFE0", "27D5E5", "4EAAEA", "7680F0", "9D55F5", "C42BFA", "EB00FF"]
+    }
+}
+// Phoenix
+presets = {
+    // Rainbow
+    1: {
+        colors: ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "9400D3"]
+    },
+    // me
+    2: {
+        colors: ["3494E6", "4B8FDF", "628BD8", "7986D1", "9081CA", "A77CC2", "BE78BB", "D573B4", "EC6EAD"]
+    },
+    // ben
+    3: {
+        colors: ["621493", "54117e", "460e69", "380b54", "2a093f", "1c062a", "0e0315", "000000"]
+    },
+    // mel
+    4: {
+        colors: ["5098ff", "3e95fe", "2d93fc", "1b90fb", "2889ec", "3481dc", "417acd"]
+    },
+    // baron
+    5: {
+        colors: ["d00000", "e4800a", "dad200", "07d003", "0014cb"]
+    },
+    // Shira
+    6: {
+        colors: ["fb48f7", "eb44ed", "da3fe2", "ca3bd8", "ba36ce", "a932c3", "992db9", "8829ae", "7824a4"]
+    },
+    // Davi
+    7: {
+        colors: ["00c0cc", "cc00c4", "cc00c4"]
+    },
+    // Cookei
+    8: {
+        colors: ["25ffe5", "1fccea", "1899ef", "1266f5", "0b33fa", "0500ff"]
+    },
+    // Crock
+    9: {
+        colors: ["ff2aea", "ff3fb0", "ff5575", "ff6a3b", "ff7f00"]
+    },
+    //Scrooge 
+    10: {
+        colors: ["fb0000", "fc1d05", "fc390a", "fd5610", "fe7313", "fe8f1a", "ffac1f"]
+    },
+    //Mochi
+    11: {
+        colors: ["90d35b", "a6de75", "bce890", "d1f3aa", "e7fdc4"]
+    },
+}
+// Wyvern
+presets = {
+    // Rainbow
+    1: {
+        colors: ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "9400D3"]
+    },
+    // Blondie
+    2: {
+        colors: ["8e42fb", "9360fb", "987dfc", "9e9bfc", "a3b8fc", "a8d6fd", "adf3fd"]
+    }
+}
+// Staff
+presets = {
+    // Rainbow
+    1: {
+        colors: ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "9400D3"]
+    },
+    // Bunny
+    2: {
+        colors: ["d6d8fb", "d9d4fb", "dbd0fc", "decdfc", "9081CA", "e1c9fc", "e3c5fd", "D573B4"]
+    },
+    // Lissi
+    3: {
+        colors: ["7300e6", "9c34f6", "ae69ee", "bf9de5", "A2A9B3", "A2A9B3", "a2c7e0", "73bce4", "45b1e8"]
+    },
+    // Alpha
+    4: {
+        colors: ["fb9f01", "c07740", "84507f", "4928be", "0d00fd"]
+    },
+    // Molly
+    5: {
+        colors: ["991199", "7a0f99", "5c0d99", "3d0a99", "1f0899", "1f0899", "000699"]
+    },
+}
