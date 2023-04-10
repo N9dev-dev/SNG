d217325a9f2b9e22f0657535dc4d3b8592a794dd
N9dev-dev <81910680+N9dev-dev@users.noreply.github.com>
2023-04-10 21:27:02 +0400

    Remade

--- Gens/js/script.js
+++ Gens/js/script.js
@@ -1,6 +1,284 @@
 const nickName = document.getElementById("nickname"),
     coloredNick = document.getElementById("coloredNick"),
-    savedColors = [getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor()],
+    savedColors = ["0D6EFD", "FFFFFF", getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor()],
+    presets = {
+        // Rainbow
+        1: {
+            colors: ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "9400D3"]
+        },
+        // me
+        2: {
+            colors: ["3494E6", "4B8FDF", "628BD8", "7986D1", "9081CA", "A77CC2", "BE78BB", "D573B4", "EC6EAD"]
+        },
+        // ben
+        3: {
+            colors: ["621493", "54117e", "460e69", "380b54", "2a093f", "1c062a", "0e0315", "000000"]
+        },
+        // mel
+        4: {
+            colors: ["5098ff", "3e95fe", "2d93fc", "1b90fb", "2889ec", "3481dc", "417acd"]
+        },
+        // baron
+        5: {
+            colors: ["2005DC", "DEDBDA"]
+        },
+        // Shira
+        6: {
+            colors: ["fb48f7", "eb44ed", "da3fe2", "ca3bd8", "ba36ce", "a932c3", "992db9", "8829ae", "7824a4"]
+        },
+        // Davi
+        7: {
+            colors: ["00c0cc", "cc00c4", "cc00c4"]
+        },
+        // Crock
+        8: {
+            colors: ["ff2aea", "ff3fb0", "ff5575", "ff6a3b", "ff7f00"]
+        },
+        // Cookie
+        9: {
+            colors: ["25ffe5", "1fccea", "1899ef", "1266f5", "0b33fa", "0500ff"]
+        },
+        // mochi
+        10: {
+            colors: ["90d35b", "a6de75", "bce890", "d1f3aa", "e7fdc4"]
+        },
+        //scrooge
+        11: {
+            colors: ["fb0000", "fc1d05", "fc390a", "fd5610", "fe7313", "fe8f1a", "ffac1f"]
+        },
+        // Juanito
+        12: {
+            colors: ["34DB09", "FFFFFF", "99884B", "FFFFFF", "F90000"]
+        },
+        // Danie
+        13: {
+            colors: ["64F8C7", "F9F995"]
+        },
+        // Koen
+        14: {
+            colors: ["E00000", "B4A202"]
+        },
+        // Fancy
+        15: {
+            colors: ["F5489B", "F38DF5"]
+        },
+        // MacDaKing
+        16: {
+            colors: ["DA0101", "075EE0", "5642A0"]
+        },
+        // AlexTheFarmer
+        17: {
+            colors: ["E49801", "E49801"]
+        },
+        // Vince
+        18: {   
+            colors: ["2E0360","100B61", "005619"]
+        },
+        // hevnoorak
+        19: {
+            colors: ["3EA9DE", "A7D241", "D74E19"]
+        },
+        // Esno
+        20: {
+            colors: ["DD0F0D", "F6CB19"]
+        },
+        // iBurkie
+        21: {
+            colors: ["019800", "53F9F9"]
+        },
+        // Rapha
+        w2: {
+            colors: ["B37F4A", "822035"]
+        },
+        // BigGavvy
+        w3: {
+            colors: ["4DE44D", "DFDFDE", "E19601"]
+        },
+        // Lora
+        w4: {
+            colors: ["0F7CBB", "01C795"]
+        },
+        // Loki
+        w5: {
+            colors: ["244717", "57E019"]
+        },
+        // Wolfie10
+        w6: {
+            colors: ["070402", "101DC2", "EB11EA", "9A10F9"]
+        },
+        // Zach
+        w7: {
+            colors: ["010000", "890000"]
+        },
+        // Aarthos
+        w8: {
+            colors: ["293945", "87BAE0"]
+        },
+        // Aazronn
+        w9: {
+            colors: ["0C032D", "0C032D"]
+        },
+        // Ruubsen
+        w10: {
+            colors: ["4201CD", "160045"]
+        },
+        // Jessie
+        w11: {
+            colors: ["0BCDC4", "C5DA0D"]
+        },
+        // Omega
+        w12: {
+            colors: ["DE0704", "58F700"]
+        },
+        // Themis
+        l2: {
+            colors: ["65adcd", "75b6d2", "86bfd7", "96c8dc", "a7d1e1", "b8dae6"]
+        },
+        // Duckyy
+        l3: {
+            colors: ["F5BF48", "F7E824"]
+        },
+        // Reaperr
+        l4: {
+            colors: ["E20909", "F7F5F5", "E10808"]
+        },
+        // Vobie
+        l5: {
+            colors: ["E4C28C", "97F8F9"]
+        },
+        // FxpWilhelm
+        l6: {
+            colors: ["E40101", "E4E400"]
+        },
+        // ORSETTOOOO
+        l7: {
+            colors: ["E02020", "1E29E1", "DB0101"]
+        },
+        // denstagmer
+        l8: {
+            colors: ["B06A19", "6F61C9"]
+        },
+        // Twister
+        l9: {
+            colors: ["0F2ED6", "5899E5"]
+        },
+        // Bandito
+        l10: {
+            colors: ["4A0708", "180101"]
+        },
+        // BlackWall
+        l11: {
+            colors: ["221F1F", "5E5656"]
+        },
+        // Cookie
+        l12: {
+            colors: ["E83232", "0DF9DD"]
+        },
+        // Waffle
+        l13: {
+            colors: ["0F2ED6", "5899E5"]
+        },
+        // HEISENBERG
+        l14: {
+            colors: ["9C0909", "9C0909"]
+        },
+        // Sonny
+        l15: {
+            colors: ["304DE2", "F77ABD"]
+        },
+        // Kuduz
+        l16: {
+            colors: ["1FAC21", "2E5BE3", "652591"]
+        },
+        // Bunny
+        s2: {
+            colors: ["d6d8fb", "d9d4fb", "dbd0fc", "decdfc", "e1c9fc", "e3c5fd", "e6c1fd"]
+        },
+        // LissiGirl
+        s3: {
+            colors: ["8b00cc", "9c34f6", "ae69ee", "bf9de5", "A2A9B3", "A2A9B3", "a2c7e0", "73bce4", "45b1e8"]
+        },
+        // ben
+        o2: {
+            colors: ["621493", "54117e", "460e69", "380b54", "2a093f", "1c062a", "0e0315", "000000"]
+        },
+        // ben
+        o3: {
+            colors: ["621493", "54117e", "460e69", "380b54", "2a093f", "1c062a", "0e0315", "000000"]
+        },
+        // mel
+        o4: {
+            colors: ["5098ff", "3e95fe", "2d93fc", "1b90fb", "2889ec", "3481dc", "417acd"]
+        },
+        // baron
+        o5: {
+            colors: ["2005DC", "DEDBDA"]
+        },
+        // Shira
+        o6: {
+            colors: ["fb48f7", "eb44ed", "da3fe2", "ca3bd8", "ba36ce", "a932c3", "992db9", "8829ae", "7824a4"]
+        },
+        // Davi
+        o7: {
+            colors: ["00c0cc", "cc00c4", "cc00c4"]
+        },
+        // Crock
+        o8: {
+            colors: ["ff2aea", "ff3fb0", "ff5575", "ff6a3b", "ff7f00"]
+        },
+        // Cookie
+        o9: {
+            colors: ["25ffe5", "1fccea", "1899ef", "1266f5", "0b33fa", "0500ff"]
+        },
+        // mochi
+        o10: {
+            colors: ["90d35b", "a6de75", "bce890", "d1f3aa", "e7fdc4"]
+        },
+        //scrooge
+        o11: {
+            colors: ["fb0000", "fc1d05", "fc390a", "fd5610", "fe7313", "fe8f1a", "ffac1f"]
+        },
+        // Juanito
+        o12: {
+            colors: ["34DB09", "FFFFFF", "99884B", "FFFFFF", "F90000"]
+        },
+        // Danie
+        o13: {
+            colors: ["64F8C7", "F9F995"]
+        },
+        // Koen
+        o14: {
+            colors: ["E00000", "B4A202"]
+        },
+        // Fancy
+        o15: {
+            colors: ["F5489B", "F38DF5"]
+        },
+        // MacDaKing
+        o16: {
+            colors: ["DA0101", "075EE0", "5642A0"]
+        },
+        // AlexTheFarmer
+        o17: {
+            colors: ["E49801", "E49801"]
+        },
+        // Vince
+        o18: {   
+            colors: ["2E0360","100B61", "005619"]
+        },
+        // hevnoorak
+        o19: {
+            colors: ["3EA9DE", "A7D241", "D74E19"]
+        },
+        // Esno
+        o20: {
+            colors: ["DD0F0D", "F6CB19"]
+        },
+        // iBurkie
+        o21: {
+            colors: ["019800", "53F9F9"]
+        },
+    },
     formats = {
         0: {
             outputPrefix: "/nick ",
@@ -9,8 +287,8 @@ const nickName = document.getElementById("nickname"),
             maxLength: 256
         },
         1: {
-            outputPrefix: "/nick ",
-            template: "<#$1$2$3$4$5$6>$f$c",
+            outputPrefix: "",
+            template: "&#$1$2$3$4$5$6 $f$c",
             formatChar: "&",
             maxLength: 256
         },
@@ -20,18 +298,6 @@ const nickName = document.getElementById("nickname"),
             formatChar: "&",
             maxLength: 256
         },
-        3: {
-            outputPrefix: "",
-            template: "<#$1$2$3$4$5$6>$f$c",
-            formatChar: "&",
-            maxLength: 256
-        },
-        4: {
-            outputPrefix: "",
-            template: "{#$1$2$3$4$5$6> }$f$c",
-            formatChar: "&",
-            maxLength: 256
-        }
 
     };
 
@@ -45,12 +311,12 @@ function darkMode() {
 
 function CopyToClipboard(id)
 {
-var r = document.createRange();
-r.selectNode(document.getElementById(id));
-window.getSelection().removeAllRanges();
-window.getSelection().addRange(r);
-document.execCommand('copy');
-window.getSelection().removeAllRanges();
+    var r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
 }
 
 function getRandomHexColor() {
@@ -143,10 +409,10 @@ function updateOutputText(e) {
     let t = formats[document.getElementById("output-format").value];
     if (t.outputPrefix && (nickName.value = nickName.value.replace(/ /g, ""), nickName.value)) {
         let t = /^[0-9a-zA-Z_]+$/;
-        nickName.value.match(t) || (nickName.value = nickName.value.replace(e.data, "")), nickName.value.match(t) || (nickName.value = "WeLovDev")
+        nickName.value.match(t) || (nickName.value = nickName.value.replace(e.data, "")), nickName.value.match(t) || (nickName.value = "birdflop.com")
     }
     let o = nickName.value;
-    o || (o = "Nickname");
+    o || (o = "Type something!");
     const r = document.getElementById("bold").checked,
         a = document.getElementById("italics").checked,
         l = document.getElementById("underline").checked,
@@ -194,17 +460,6 @@ function preset(e) {
     }
     jscolor.install()
 }
-
-const DefualtFormat = (e) => {
-    const text = '/nick {#rrggbb}';
-    const $select = document.querySelector('#output-format');
-    const $options = Array.from($select.options);
-    const optionToSelect = $options.find(item => item.text ===text);
-    optionToSelect.selected = true;
-    updateOutputText();
-  };
-
-
 
 function Alert(){
     Alert("Nope.");
